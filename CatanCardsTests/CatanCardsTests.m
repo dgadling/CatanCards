6c6166af2187a837e6fb86d24ef216b93d6b9beb
Dave Gadling <dgadling@fb.com>
2014-11-26 16:13:49 -0800

    Initial Commit

---
+++ CatanCardsTests/CatanCardsTests.m
@@ -0,0 +1,40 @@
+//
+//  CatanCardsTests.m
+//  CatanCardsTests
+//
+//  Created by Dave Gadling on 11/26/14.
+//  Copyright (c) 2014 Toasterwaffles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CatanCardsTests : XCTestCase
+
+@end
+
+@implementation CatanCardsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
